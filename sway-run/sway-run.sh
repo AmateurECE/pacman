67b7bc23d7c35cc9d1419e1430ec8d6fa6863c3c
Ethan D. Twardy <ethan.twardy@gmail.com>
2022-08-12 14:30:38 -0500

    Disable hardware cursors in wlroots

--- sway-run/sway-run.sh
+++ sway-run/sway-run.sh
@@ -8,13 +8,17 @@
 #
 # CREATED:          07/22/2022
 #
-# LAST EDITED:      07/22/2022
+# LAST EDITED:      08/12/2022
 ###
 
 export XDG_SESSION_TYPE=wayland
 export XDG_SESSION_DESKTOP=sway
 export XDG_CURRENT_DESKTOP=sway
 
-systemd-cat --identifier=sway sway -d
+# This one is a temporary bandaid for the M1, since it seems like sway doesn't
+# play nicely with the simple-framebuffer DRM driver.
+export WLR_NO_HARDWARE_CURSORS=1
+
+systemd-cat --identifier=sway sway
 
 ###############################################################################
