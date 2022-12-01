681b2e70128fe2d41dffaa2da3e973280513ab8e
Ethan D. Twardy <ethan.twardy@gmail.com>
2022-11-30 22:52:41 -0600

    Fix startwm package and remove a lot of complexity

--- startwm-env-m1/startwm.sh
+++
@@ -1,4 +0,0 @@
-# This is needed to run wlroots-based window managers in some virtual machine
-# environments (such as VMWare).
-export WLR_RENDERER_ALLOW_SOFTWARE=1
-export STARTWM_SHELL=hyprland
--- startwm-env-vm/startwm.sh
+++
@@ -1,4 +0,0 @@
-# This is needed to run wlroots-based window managers in some virtual machine
-# environments (such as VMWare).
-export WLR_NO_HARDWARE_CURSORS=1
-export STARTWM_SHELL=hyprland
